37b0bece411c3fa9b1309e77d6f4fcdc78ffb558
lokeshvaishnav11 <lokeshkumariit@gmail.com>
2026-01-21 20:59:45 +0530

    FGHJK

--- frontend/src/pages/odds/odds.tsx
+++ frontend/src/pages/odds/odds.tsx
@@ -166,7 +166,8 @@ const Odds = () => {
       return (
         <iframe
           style={{ width: "100%", height: "auto" }}
-          src={`https://card.hr08bets.in/api/getScoreData?event_id=${currentMatch?.matchId}`}
+          // src={`https://card.hr08bets.in/api/getScoreData?event_id=${currentMatch?.matchId}`}
+          src={`https://score.akamaized.uk/?id=${currentMatch?.matchId}`}
         ></iframe>
       );
   };
