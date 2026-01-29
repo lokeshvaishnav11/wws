08313302348f3def3b23dd9443cf222f2deca8f7
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2026-01-29 18:50:12 +0530

    Update UserBookController.ts

--- backend/src/controllers/UserBookController.ts
+++ backend/src/controllers/UserBookController.ts
@@ -159,9 +159,9 @@ export class UserBookController extends ApiController {
 
     for (const item of betlist) {
       const childId = getChildId(item.parentStr)
-      if (!childId) continue
+     // if (!childId) continue
 
-      const childShare = childShareMap[childId.toString()] || 0
+      const childShare = childShareMap[childId?.toString()] || 0
       const shareFactor = (currentShare - childShare) / 100
 
       if (shareFactor === 0) continue
